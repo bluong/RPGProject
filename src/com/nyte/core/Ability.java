fa0726a315b35e11e1d50a761b2b26cc1c6eb9d5
bluong <bluong@berkeley.edu>
2013-12-09 05:26:41 -0800

    Ability DS

--- src/com/nyte/core/Ability.java
+++ src/com/nyte/core/Ability.java
@@ -4,4 +4,30 @@ package com.nyte.core;
  * Created by Brandon on 12/2/13.
  */
 public class Ability {
+	
+	private Target target;
+	private Attributes.Stat costStat;
+	private int cost;
+	private Attributes.Stat affectingStat;
+	private int value;
+	
+	public Target getTarget() {
+		return target;
+	}
+	
+	public Attributes.Stat getCostingStat() {
+		return costStat;
+	}
+	
+	public int getCost() {
+		return cost;
+	}
+	
+	public Attributes.Stat getAffectingStat() {
+		return affectingStat;
+	}
+	
+	public int getValue() {
+		return value;
+	}
 }
