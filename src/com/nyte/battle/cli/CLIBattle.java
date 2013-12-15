a508da4481dca881bc51b3f093ab5cfaf7cf0dc0
bluong <bluong@berkeley.edu>
2013-12-15 08:05:25 -0800

    Setting actionCommander on inst

--- src/com/nyte/battle/cli/CLIBattle.java
+++ src/com/nyte/battle/cli/CLIBattle.java
@@ -1,13 +1,14 @@
 package com.nyte.battle.cli;
 
 import com.nyte.battle.Battle;
+import com.nyte.battle.cli.commander.CLIAbilityCommander;
 import com.nyte.core.Party;
 
 public class CLIBattle extends Battle {
 
 	public CLIBattle(Party p1, Party p2) {
 	    super(p1, p2);
-	    // TODO Auto-generated constructor stub
+	    actionCommander = new CLIAbilityCommander(this);
     }
 
 }
